d82a78132ac169a0e6040a01f2828513473d8262
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-24 15:09:47 +0300

    Added controller class ProductController

---
+++ sevices/product-service/src/main/java/com/kolos/productservice/web/ProductController.java
@@ -0,0 +1,53 @@
+package com.kolos.productservice.web;
+
+import com.kolos.productservice.service.ProductService;
+import com.kolos.productservice.service.dto.ProductPurchaseRequest;
+import com.kolos.productservice.service.dto.ProductPurchaseResponse;
+import com.kolos.productservice.service.dto.ProductRequest;
+import com.kolos.productservice.service.dto.ProductResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+
+
+    @PostMapping
+    public ResponseEntity<Integer> createProduct(
+            @RequestBody @Valid ProductRequest request) {
+        return ResponseEntity.ok(productService.createProduct(request));
+    }
+
+    @PostMapping("/purchase")
+    public ResponseEntity<List<ProductPurchaseResponse>> purchaseProduct(
+            @RequestBody @Valid List<ProductPurchaseRequest> request
+    ) {
+        return ResponseEntity.ok(productService.purchaseProducts(request));
+    }
+
+    @GetMapping("/{product-id}")
+    public ResponseEntity<ProductResponse> findById(
+            @PathVariable("product-id") Integer productId
+    ) {
+        return ResponseEntity.ok(productService.findById(productId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponse>> findAll() {
+        return ResponseEntity.ok(productService.findAll());
+    }
+}
