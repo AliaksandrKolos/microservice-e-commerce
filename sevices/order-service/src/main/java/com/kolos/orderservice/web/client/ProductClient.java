13fc713a69cdde28986f9665df02f784b644e35f
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-26 11:34:33 +0300

    Added client for sending messages to product-service

---
+++ sevices/order-service/src/main/java/com/kolos/orderservice/web/client/ProductClient.java
@@ -0,0 +1,47 @@
+package com.kolos.orderservice.web.client;
+
+import com.kolos.orderservice.service.dto.PurchaseRequest;
+import com.kolos.orderservice.web.exception.BusinessException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import org.springframework.http.HttpHeaders;
+import java.util.List;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+
+@Service
+@RequiredArgsConstructor
+public class ProductClient {
+
+    @Value("${application.config.product-url}")
+    private String productUrl;
+    private final RestTemplate restTemplate;
+
+    public List<PurchaseResponse> purchaseProducts(List<PurchaseRequest> requestsBody) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+
+        HttpEntity<List<PurchaseRequest>> requestEntity = new HttpEntity<>(requestsBody, headers);
+        ParameterizedTypeReference<List<PurchaseResponse>> responseType = new ParameterizedTypeReference<>() {};
+        ResponseEntity<List<PurchaseResponse>> responseEntity = restTemplate.exchange(
+                productUrl + "/purchase",
+                POST,
+                requestEntity,
+                responseType
+        );
+
+        if (responseEntity.getStatusCode().isError()) {
+            throw new BusinessException("An error occurred while processing the products purchase: " + responseEntity.getStatusCode());
+        }
+        return  responseEntity.getBody();
+    }
+}
