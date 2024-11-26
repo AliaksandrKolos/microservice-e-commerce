85fdcc47caca1dd1d3c95dae7e92e694b8934bf2
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-26 14:25:53 +0300

    Added new service: notification-service

---
+++ sevices/notofocation-service/src/main/java/com/kolos/notofocationservice/NotofocationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kolos.notofocationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotofocationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotofocationServiceApplication.class, args);
+	}
+
+}
