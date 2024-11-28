7933096283a20bd8f47420d2a43ddf95271d1e92
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-28 09:54:16 +0300

    Placed rabbitMQ variables in yml file

--- sevices/payment-service/src/main/java/com/kolos/paymentservice/service/impl/MessagePublisherImpl.java
+++ sevices/payment-service/src/main/java/com/kolos/paymentservice/service/impl/MessagePublisherImpl.java
@@ -5,6 +5,7 @@ import com.kolos.paymentservice.service.dto.PaymentNotificationRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Slf4j
@@ -14,12 +15,20 @@ public class MessagePublisherImpl implements MessagePublisher {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Value("${rabbitmq.exchange.direct}")
+    private String directExchangeName;
+
+
+    @Value("${rabbitmq.routing-key.payment-notification}")
+    private String paymentNotificationRoutingKey;
+
+
+
     @Override
     public void sendNotification(PaymentNotificationRequest request) {
         log.info("Sending notification with body <{}>", request);
-        rabbitTemplate.convertAndSend(request);
+        rabbitTemplate.convertAndSend(directExchangeName, paymentNotificationRoutingKey, request);
     }
-
 
 
 }
