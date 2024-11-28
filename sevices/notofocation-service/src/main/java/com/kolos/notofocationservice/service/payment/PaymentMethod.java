6d8b8cc468c902645f88ae33b53b1ef127164384
Alexander Kolosovsky <kolosovskii.201416@gmail.com>
2024-11-28 09:47:49 +0300

    Added annotations for class deserialization

--- sevices/notofocation-service/src/main/java/com/kolos/notofocationservice/service/payment/PaymentMethod.java
+++ sevices/notofocation-service/src/main/java/com/kolos/notofocationservice/service/payment/PaymentMethod.java
@@ -1,10 +1,29 @@
 package com.kolos.notofocationservice.service.payment;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum PaymentMethod {
 
     PAYPAL,
     CREDIT_CARD,
     VISA,
     MASTER_CARD,
-    BITCOIN
+    BITCOIN;
+
+    @JsonValue
+    public String getValue() {
+        return name();
+    }
+
+    @JsonCreator
+    public static PaymentMethod fromValue(String value) {
+        for (PaymentMethod method : PaymentMethod.values()) {
+            if (method.name().equalsIgnoreCase(value)) {
+                return method;
+            }
+        }
+        throw new IllegalArgumentException("Unknown payment method: " + value);
+    }
 }
+
